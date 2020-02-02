1e7c76fa808b0042fa2fa4b3519ea879611c2797
mocobt <mocobt@gmail.com>
2020-02-02 18:01:30 +0900

    rm if

--- pipeline/pipeline/models/pretrained_cnn.py
+++ pipeline/pipeline/models/pretrained_cnn.py
@@ -2,7 +2,6 @@ import pretrainedmodels
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torch.nn import Sequential
 
 from .linear_block import LinearBlock
 
@@ -10,27 +9,18 @@ from .linear_block import LinearBlock
 class PretrainedCNN(nn.Module):
     def __init__(self, model_name='se_resnext101_32x4d',
                  in_channels=3, out_dim=10, hdim=512, activation=F.leaky_relu,
-                 use_bn=True, pretrained='imagenet', do_pooling=True):
+                 use_bn=True, pretrained='imagenet'):
         super(PretrainedCNN, self).__init__()
-        self.conv0 = nn.Conv2d(
-            in_channels, 3, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv0 = nn.Conv2d(in_channels, 3, kernel_size=3, stride=1, padding=1, bias=True)
         self.base_model = pretrainedmodels.__dict__[model_name](pretrained=pretrained)
-        self.do_pooling = do_pooling
-        inch = self.base_model.last_linear.in_features if self.do_pooling else None
-        lin1 = LinearBlock(inch, hdim, use_bn=use_bn, activation=activation, residual=False)
-        lin2 = LinearBlock(hdim, out_dim, use_bn=use_bn, activation=None, residual=False)
-        self.lin_layers = Sequential(lin1, lin2)
+        inch = self.base_model.last_linear.in_features
+        self.lin1 = LinearBlock(inch, hdim, use_bn=use_bn, activation=activation, residual=False)
+        self.lin2 = LinearBlock(hdim, out_dim, use_bn=use_bn, activation=None, residual=False)
 
     def forward(self, x):
         h = self.conv0(x)
         h = self.base_model.features(h)
-
-        if self.do_pooling:
-            h = torch.sum(h, dim=(-1, -2))
-        else:
-            # [128, 2048, 4, 4] when input is (128, 128)
-            bs, ch, height, width = h.shape
-            h = h.view(bs, ch * height * width)
-        for layer in self.lin_layers:
-            h = layer(h)
+        h = torch.sum(h, dim=(-1, -2))
+        h = self.lin1(h)
+        h = self.lin2(h)
         return h
