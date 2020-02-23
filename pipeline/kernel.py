575e790c627e5452278384d2324fc201fa494af3
mocobt <mocobt@gmail.com>
2020-02-24 03:00:23 +0900

    fix mlflow

--- pipeline/kernel.py
+++ pipeline/kernel.py
@@ -1,14 +1,12 @@
 import importlib
-from pathlib import Path
 from .pipeline import utils
 
 
 class Kernel():
-    def __init__(self, competition, index, input_path, weight_path):
-        config_path = Path(".").resolve() / "config"
-        cfg = utils.read_yaml(config_path / competition / f"{index}.yaml")
+    def __init__(self, competition, input_path, config_path, competition_yaml_path, weight_path):
+        cfg = utils.read_yaml(config_path)
         utils.seed_everything(cfg["seed"])
-        competition_dict = utils.read_yaml(Path(".").resolve() / "competition.yaml")
+        competition_dict = utils.read_yaml(competition_yaml_path)
         module_name = "{}Kernel".format(competition_dict[competition])
         modulelib = importlib.import_module("pipeline.{}".format(module_name))
         self.model = getattr(modulelib, module_name)(competition, cfg, input_path, weight_path)
