0e9d9162ba1e9792a4ec328e9047e3da29f08221
mocobt <mocobt@gmail.com>
2020-03-15 04:33:07 +0900

    fix mixup

--- competition/Bengali.py
+++ competition/Bengali.py
@@ -6,6 +6,7 @@ import zipfile
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import cv2
 import torch
 import torch.nn as nn
@@ -59,22 +60,17 @@ def rand_bbox(size, lam):
     W = size[2]
     H = size[3]
     cut_rat = np.sqrt(1. - lam)
-    cut_w = np.int(W * cut_rat)
     cut_h = np.int(H * cut_rat)
 
     # uniform
-    cx = np.random.randint(W)
-    cy = np.random.randint(H)
-
-    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    cy = np.random.randint(int(H / 5), int(4 * H / 5))
     bby1 = np.clip(cy - cut_h // 2, 0, H)
-    bbx2 = np.clip(cx + cut_w // 2, 0, W)
     bby2 = np.clip(cy + cut_h // 2, 0, H)
 
-    return bbx1, bby1, bbx2, bby2
+    return 0, bby1, W, bby2
 
 
-def bengali_cutmix_or_mixup(data, targets, alpha=0.2, is_cutmix=True, use_all=False):
+def bengali_cutmix_or_mixup(data, targets, is_cutmix=True, use_all=False):
     # cutmix if is_cutmix else mixup
     indices = torch.randperm(data.size(0))
     shuffled_data = data[indices]
@@ -82,10 +78,10 @@ def bengali_cutmix_or_mixup(data, targets, alpha=0.2, is_cutmix=True, use_all=Fa
     shuffled_targets1 = targets[:, 1][indices]
     shuffled_targets2 = targets[:, 2][indices]
 
-    lam = np.random.beta(alpha, alpha)
+    lam = np.random.uniform(0, 1.0)
     if is_cutmix:
         bbx1, bby1, bbx2, bby2 = rand_bbox(data.size(), lam)
-        data[:, :, bbx1:bbx2, bby1:bby2] = data[indices, :, bbx1:bbx2, bby1:bby2]
+        data[:, :, bbx1:bbx2, bby1:bby2] = shuffled_data[indices, :, bbx1:bbx2, bby1:bby2]
         lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (data.size()[-1] * data.size()[-2]))
     else:
         data = data * lam + shuffled_data * (1 - lam)
@@ -233,9 +229,9 @@ class Bengali():
         for inputs, labels, _ in tqdm(self.train_loader):
             inputs, labels = inputs.to(self.device), labels.to(self.device)
             inputs, labels, lam = bengali_cutmix_or_mixup(inputs, labels,
-                                                          alpha=self.cfg["others"]["alpha"],
                                                           is_cutmix=self.cfg["others"]["name"] == "cutmix",
                                                           use_all=self.use_grapheme)
+            inputs = self.train_loader.transform(inputs)
             self.optimizer.zero_grad()
             preds = self.model(inputs)
             if isinstance(preds, tuple) is False:
